cb58fe9112059a12d220f106a8e481e59b0b6ebc
joshzheng777 <joshzheng77@gmail.com>
2025-06-28 12:42:59 -0400

    Added basic TailwindCSS

--- client/src/Navbar.tsx
+++ client/src/Navbar.tsx
@@ -1,14 +1,28 @@
-const Navbar = () => {
+
+
+const Navbar = () => {    
     return(
         <>
-            <ul>
-                <li><a href="#">Website Logo</a></li>
-                <li><a href="#">Movies</a></li>
-                <li><a href="#">Account</a></li>
-                <li><a href="#">About</a></li>
-            </ul>
-            <h1>Welcome to Movie Reviews</h1>
-            <p>Search</p>
+            <nav className="bg-gray-800 text-white p-4">
+                <ul className="flex space-x-4 items-center">
+                    <li>
+                        <a href="#" className="font-bold text-xl">Website Logo</a>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:text-gray-300">Movies</a>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:text-gray-300">Account</a>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:text-gray-300">About</a>
+                    </li>
+                </ul>
+            </nav>
+            <div className="p-4">
+                <h1 className="text-2xl font-semibold">Welcome to Movie Reviews</h1>
+                <p>Search</p>
+            </div>
         </>
     )
 }
