1e73bcdafb361311060fbac5ea4496e7fdc5fecc
joshzheng777 <joshzheng77@gmail.com>
2025-07-16 22:08:36 -0400

    Replaced static card display with JS

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,12 +12,24 @@ const App = () => {
     interface Movie {
         title: string;
         poster_path: string;
+        rating: number;
     }
 
     const [popularMoviesList, setPopularMoviesList] = useState<Movie[]>([
         {
             title: "Man of Steel",
-            poster_path: "../assets/man-of-steel.jpg"
+            poster_path: "../assets/man-of-steel.jpg",
+            rating: 5
+        },
+        {
+            title: "Man of Steel",
+            poster_path: "../assets/man-of-steel.jpg",
+            rating: 5
+        },
+        {
+            title: "Man of Steel",
+            poster_path: "../assets/man-of-steel.jpg",
+            rating: 5
         }
     ])
 
@@ -28,10 +40,14 @@ const App = () => {
             <Welcome isLoggedIn={true} />
             <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
             <div className="flex flex-wrap justify-center gap-4">
+                {/* <Card title="Man of Steel" poster_path="../assets/man-of-steel.jpg" />
                 <Card title="Man of Steel" poster_path="../assets/man-of-steel.jpg" />
                 <Card title="Man of Steel" poster_path="../assets/man-of-steel.jpg" />
-                <Card title="Man of Steel" poster_path="../assets/man-of-steel.jpg" />
-                <Card title="Man of Steel" poster_path="../assets/man-of-steel.jpg" />
+                <Card title="Man of Steel" poster_path="../assets/man-of-steel.jpg" /> */}
+
+                {popularMoviesList.map((movie) => (
+                    <Card title={movie.title} poster_path={movie.poster_path} />
+                ))}
             </div>
         </main>
         <Footer />
