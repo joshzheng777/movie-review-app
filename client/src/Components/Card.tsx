0c3e697dc912509bddc774e75ff5cc197c137787
joshzheng777 <joshzheng77@gmail.com>
2025-07-30 00:46:48 -0400

    Fixed Water Later CSS. Added More Info button

--- client/src/Components/Card.tsx
+++ client/src/Components/Card.tsx
@@ -10,12 +10,17 @@ const Card = ({ title, poster_path, rating }: CardProps) => {
     }
 
     return (
-        <div className="max-w-[18rem] rounded-lg overflow-hidden shadow-lg border-2 border-gray-300">
+        <div className="max-w-[18rem] rounded-lg overflow-hidden shadow-lg border-2 border-gray-300 flex flex-col">
             <img className="aspect-2/3 object-cover" src={poster_path} alt="poster" />
-            <div className="px-6 py-4">
-                <p className="font-bold text-xl mb-2 text-center">{title}</p>
-                <p className="text-gray-700 text-base text-center">⭐ {rating}</p>
+            <div className="px-6 py-4 flex flex-col flex-grow">
+                <div className="flex-grow">
+                    <p className="font-bold text-xl mb-2 text-center">{title}</p>
+                    <p className="text-gray-700 text-base text-center">⭐ {rating}</p>
+                </div>
                 <div className="text-center mt-4">
+                    <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mb-2">
+                        More Info
+                    </button>
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                         Add to Watch Later
                     </button>
